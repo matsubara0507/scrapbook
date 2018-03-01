6b3afbd5b6889df38a77f1ba9d5ff6a6c9497cba
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2018-03-02 02:50:59 +0900

    [WIP] add error

--- src/ScrapBook/Fetch.hs
+++ src/ScrapBook/Fetch.hs
@@ -1,21 +1,23 @@
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedLabels  #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PolyKinds         #-}
 {-# LANGUAGE TypeOperators     #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module ScrapBook.Fetch where
 
-import           Control.Lens             ((^.))
+import           Control.Lens              ((^.))
+import           Control.Monad.Error.Class (throwError)
 import           Data.Extensible
 import           Data.Functor.Identity
-import           Data.Proxy               (Proxy (..))
-import           Data.Text                (Text)
+import           Data.Proxy                (Proxy (..))
+import           Data.Text                 (Text)
 import           ScrapBook.Collecter
 import           ScrapBook.Data.Site
-import           ScrapBook.Feed           ()
-import           ScrapBook.Fetch.Internal (Fetch (..))
+import           ScrapBook.Feed            ()
+import           ScrapBook.Fetch.Internal  (Fetch (..))
 
 fetch :: Site -> Collecter [Post]
 fetch site = flip matchField (site ^. #id) $
@@ -23,4 +25,4 @@ fetch site = flip matchField (site ^. #id) $
     \m -> Field (Match $ fetchFrom m site . runIdentity)
 
 instance Fetch ("url" >: Text) where
-  fetchFrom _ _ _ = pure []
+  fetchFrom _ _ _ = throwError $ CollectException "undefined fetching behavior."
